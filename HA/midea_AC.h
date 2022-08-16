dd9ded8c81b47dfa99ecd42addfb4f8debbf964b
rikman122 <rikipr_12@hotmail.com>
2022-08-16 21:47:51 +0200

    Revert "clean"
    This reverts commit 74f918988dd385e10238d82c5e48aca91aff3d52.

--- HA/midea_AC.h
+++ HA/midea_AC.h
@@ -3,16 +3,78 @@
 
 #define SERIAL_COM_CONTROL_PIN 4
 #define SERIAL_COM_MASTER_ID 0
-#define SERIAL_COM_SLAVE_ID 0xFF
+#define SERIAL_COM_SLAVE_ID 0
 #define SERIAL_COM_MASTER_SEND_TIME 30
 #define SERIAL_COM_SLAVE_TIMEOUT_TIME 100
 
 float DesiredTemp = 18;
 
-class MideaACSensor : public PollingComponent
+class MideaACSensor : public PollingComponent, public Sensor
 {
 public:
-    MideaACSensor() : PollingComponent(10000){}
+    // constructor
+    Sensor *ACT1Temp = new Sensor();
+    Sensor *ACT2ATemp = new Sensor();
+    Sensor *ACT2BTemp = new Sensor();
+    Sensor *ACT3Temp = new Sensor();
+    Sensor *ACNotResponding = new Sensor();
+    std::string SetMode = "Unknown";
+    std::string SetFanMode = "Unknown";
+    uint8_t SetTemp = 18;
+    bool aux_heat = 0;
+    bool echo_sleep = 0;
+    bool vent = 0;
+    bool swing = 0;
+    bool lock = 0;
+
+    esphome::template_::TemplateSelect *_SetMode;
+    esphome::template_::TemplateSelect *_SetFanMode;
+    esphome::template_::TemplateNumber *_SetTemp;
+    esphome::template_::TemplateSwitch *_aux_heat;
+    esphome::template_::TemplateSwitch *_echo_sleep;
+    esphome::template_::TemplateSwitch *_vent;
+    esphome::template_::TemplateSwitch *_swing;
+    esphome::template_::TemplateSwitch *_lock;
+
+    uint8_t update_command = 0;
+    uint8_t update_internal = 0;
+
+    MideaACSensor(
+        esphome::template_::TemplateSelect *&_SetMode_in,
+        esphome::template_::TemplateSelect *&_SetFanMode_in,
+        esphome::template_::TemplateNumber *&_SetTemp_in,
+        esphome::template_::TemplateSwitch *&_aux_heat_in,
+        esphome::template_::TemplateSwitch *&_echo_sleep_in,
+        esphome::template_::TemplateSwitch *&_vent_in,
+        esphome::template_::TemplateSwitch *&_swing_in,
+        esphome::template_::TemplateSwitch *&_lock_in) : PollingComponent(10000)
+    {
+        _SetMode = _SetMode_in;
+        _SetFanMode = _SetFanMode_in;
+        _SetTemp = _SetTemp_in;
+        _aux_heat = _aux_heat_in;
+        _echo_sleep = _echo_sleep_in;
+        _vent = _vent_in;
+        _swing = _swing_in;
+        _lock = _lock_in;
+
+        _SetMode->add_on_state_callback([this](std::string value, size_t size)
+                                        { if((1 != update_internal)&&(value != SetMode)){update_command =1;} });
+        _SetFanMode->add_on_state_callback([this](std::string value, size_t size)
+                                           { if((1 != update_internal)&&(value != SetFanMode)){update_command =1;} });
+        _SetTemp->add_on_state_callback([this](float value)
+                                        { if((1 != update_internal)&&(value != SetTemp)){update_command =1;} });
+        _aux_heat->add_on_state_callback([this](bool value)
+                                         { if((1 != update_internal)&&(value != aux_heat)){update_command =1;} });
+        _echo_sleep->add_on_state_callback([this](bool value)
+                                           { if((1 != update_internal)&&(value != echo_sleep)){update_command =1;} });
+        _swing->add_on_state_callback([this](bool value)
+                                      { if((1 != update_internal)&&(value != swing)){update_command =1;} });
+        _vent->add_on_state_callback([this](bool value)
+                                     { if((1 != update_internal)&&(value != vent)){update_command =1;} });
+        _lock->add_on_state_callback([this](bool value)
+                                     { if(value == 1){ESP8266_Midea_RS485.Lock();}else{ESP8266_Midea_RS485.Unlock();} });
+    }
 
     float get_setup_priority() const override { return esphome::setup_priority::AFTER_WIFI; }
 
@@ -22,6 +84,103 @@ public:
     }
     void update() override
     {
+
+        float temp;
+        uint8_t index;
+
+        if (1 == update_command)
+        {
+            SetMode = _SetMode->state.c_str();
+
+            if (SetMode == "Auto")
+            {
+                ESP8266_Midea_RS485.SetMode(MIDEA_AC_OPMODE_AUTO);
+            }
+            if (SetMode == "Off")
+            {
+                ESP8266_Midea_RS485.SetMode(MIDEA_AC_OPMODE_OFF);
+            }
+            if (SetMode == "Cool")
+            {
+                ESP8266_Midea_RS485.SetMode(MIDEA_AC_OPMODE_COOL);
+            }
+            if (SetMode == "Heat")
+            {
+                ESP8266_Midea_RS485.SetMode(MIDEA_AC_OPMODE_HEAT);
+            }
+            if (SetMode == "Dry")
+            {
+                ESP8266_Midea_RS485.SetMode(MIDEA_AC_OPMODE_DRY);
+            }
+            if (SetMode == "Fan")
+            {
+                ESP8266_Midea_RS485.SetMode(MIDEA_AC_OPMODE_FAN);
+            }
+
+            SetFanMode = _SetFanMode->state.c_str();
+
+            if (SetFanMode == "Auto")
+            {
+                ESP8266_Midea_RS485.SetFanMode(MIDEA_AC_FANMODE_AUTO);
+            }
+            if (SetFanMode == "High")
+            {
+                ESP8266_Midea_RS485.SetFanMode(MIDEA_AC_FANMODE_HIGH);
+            }
+            if (SetFanMode == "Medium")
+            {
+                ESP8266_Midea_RS485.SetFanMode(MIDEA_AC_FANMODE_MEDIUM);
+            }
+            if (SetFanMode == "Low")
+            {
+                ESP8266_Midea_RS485.SetFanMode(MIDEA_AC_FANMODE_LOW);
+            }
+
+            SetTemp = (_SetTemp->state) / 1;
+            ESP8266_Midea_RS485.SetTemp(SetTemp);
+
+            aux_heat = _aux_heat->state;
+            if (!aux_heat)
+            {
+                ESP8266_Midea_RS485.SetAuxHeat_Turbo(MIDEA_AC_ACTIVE);
+            }
+            else
+            {
+                ESP8266_Midea_RS485.SetAuxHeat_Turbo(MIDEA_AC_INACTIVE);
+            }
+
+            echo_sleep = _echo_sleep->state;
+            if (!echo_sleep)
+            {
+                ESP8266_Midea_RS485.SetEcho_Sleep(MIDEA_AC_ACTIVE);
+            }
+            else
+            {
+                ESP8266_Midea_RS485.SetEcho_Sleep(MIDEA_AC_INACTIVE);
+            }
+
+            vent = _vent->state;
+            if (!vent)
+            {
+                ESP8266_Midea_RS485.SetVent(MIDEA_AC_ACTIVE);
+            }
+            else
+            {
+                ESP8266_Midea_RS485.SetVent(MIDEA_AC_INACTIVE);
+            }
+
+            swing = _swing->state;
+            if (!swing)
+            {
+                ESP8266_Midea_RS485.SetSwing(MIDEA_AC_ACTIVE);
+            }
+            else
+            {
+                ESP8266_Midea_RS485.SetSwing(MIDEA_AC_INACTIVE);
+            }
+
+            update_command = 2;
+        }
 
         ESP8266_Midea_RS485.Update();
 
@@ -88,5 +247,83 @@ public:
         {
             ESP8266_Midea_RS485.ReceivedData[index] = 0;
         }
+
+        if (ESP8266_Midea_RS485.State.ACNotResponding == 0)
+        {
+            update_internal = 1;
+
+            if (ESP8266_Midea_RS485.State.OpMode == MIDEA_AC_OPMODE_AUTO)
+            {
+                _SetMode->publish_state("Auto");
+            }
+            else if (ESP8266_Midea_RS485.State.OpMode == MIDEA_AC_OPMODE_OFF)
+            {
+                _SetMode->publish_state("Off");
+            }
+            else if (ESP8266_Midea_RS485.State.OpMode == MIDEA_AC_OPMODE_COOL)
+            {
+                _SetMode->publish_state("Cool");
+            }
+            else if (ESP8266_Midea_RS485.State.OpMode == MIDEA_AC_OPMODE_HEAT)
+            {
+                _SetMode->publish_state("Heat");
+            }
+            else if (ESP8266_Midea_RS485.State.OpMode == MIDEA_AC_OPMODE_DRY)
+            {
+                _SetMode->publish_state("Dry");
+            }
+            else if (ESP8266_Midea_RS485.State.OpMode == MIDEA_AC_OPMODE_FAN)
+            {
+                _SetMode->publish_state("Fan");
+            }
+            else
+            {
+                _SetMode->publish_state("Unknown");
+            }
+
+            if (ESP8266_Midea_RS485.State.FanMode == MIDEA_AC_FANMODE_AUTO)
+            {
+                _SetFanMode->publish_state("Auto");
+            }
+            else if (ESP8266_Midea_RS485.State.FanMode == MIDEA_AC_FANMODE_HIGH)
+            {
+                _SetFanMode->publish_state("High");
+            }
+            else if (ESP8266_Midea_RS485.State.FanMode == MIDEA_AC_FANMODE_MEDIUM)
+            {
+                _SetFanMode->publish_state("Medium");
+            }
+            else if (ESP8266_Midea_RS485.State.FanMode == MIDEA_AC_FANMODE_LOW)
+            {
+                _SetFanMode->publish_state("Low");
+            }
+            else
+            {
+                _SetFanMode->publish_state("Unknown");
+            }
+
+            if (ESP8266_Midea_RS485.State.SetTemp > 0)
+            {
+                temp = ESP8266_Midea_RS485.State.SetTemp * 1.0;
+                _SetTemp->publish_state(temp);
+            }
+            else
+            {
+                _SetTemp->publish_state(18);
+            }
+
+            _aux_heat->publish_state((ESP8266_Midea_RS485.State.OperatingFlags & 0x02) > 0);
+            _echo_sleep->publish_state((ESP8266_Midea_RS485.State.OperatingFlags & 0x01) > 0);
+            _vent->publish_state((ESP8266_Midea_RS485.State.OperatingFlags & 0x88) > 0);
+            _swing->publish_state((ESP8266_Midea_RS485.State.OperatingFlags & 0x04) > 0);
+
+            update_internal = 0;
+        }
+
+        ACT1Temp->publish_state(ESP8266_Midea_RS485.State.T1Temp);
+        ACT2ATemp->publish_state(ESP8266_Midea_RS485.State.T2ATemp);
+        ACT2BTemp->publish_state(ESP8266_Midea_RS485.State.T2BTemp);
+        ACT3Temp->publish_state(ESP8266_Midea_RS485.State.T3Temp);
+        ACNotResponding->publish_state(ESP8266_Midea_RS485.State.ACNotResponding);
     }
 };
--- src/esp8266_midea_RS485.h
+++ src/esp8266_midea_RS485.h
@@ -4,8 +4,69 @@
 
 #ifndef _esp8266_midea_rs485_h
     #define _esp8266_midea_rs485_h
+    
     #include "Arduino.h"
     
+    typedef enum{
+      MIDEA_AC_OPMODE_OFF=0,
+      MIDEA_AC_OPMODE_AUTO,
+      MIDEA_AC_OPMODE_COOL,
+      MIDEA_AC_OPMODE_DRY,
+      MIDEA_AC_OPMODE_HEAT,
+      MIDEA_AC_OPMODE_FAN,
+      MIDEA_AC_OPMODE_UNKOWN
+    }MideaACOpModeType;
+
+    typedef enum{
+      MIDEA_AC_FANMODE_AUTO=0,
+      MIDEA_AC_FANMODE_HIGH,
+      MIDEA_AC_FANMODE_MEDIUM,
+      MIDEA_AC_FANMODE_LOW,
+      MIDEA_AC_FANMODE_UNKNOWN
+    }MideaACFanModeType;
+    
+    typedef enum{
+      MIDEA_AC_ACTIVE=0,
+      MIDEA_AC_INACTIVE=1,
+    }MideaACOpFeatureStateType;
+
+    typedef struct{
+      uint8_t Unknown1;
+      uint8_t Capabilities;
+      MideaACOpModeType OpMode;
+      MideaACFanModeType FanMode;
+      uint8_t SetTemp;
+      int T1Temp;
+      int T2ATemp;
+      int T2BTemp;
+      int T3Temp;
+      uint8_t Current;
+      uint8_t Unknown2;
+      uint32_t TimerStart;
+      uint32_t TimerStop;
+      uint8_t Unknown3;
+      uint8_t ModeFlags;
+      uint8_t OperatingFlags;
+      uint16_t ErrorFlags;
+      uint16_t ProtectFlags;
+      uint8_t CCMComErrorFlags;
+      uint8_t Unknown4;
+      uint8_t Unknown5;    
+      uint8_t ACNotResponding;    
+    }MideaACPropertiesType;
+
+    typedef struct{
+      MideaACOpModeType OpMode;
+      MideaACFanModeType FanMode;
+      uint8_t SetTemp;
+      MideaACOpFeatureStateType AuxHeat_Turbo;
+      MideaACOpFeatureStateType Echo_Sleep;
+      MideaACOpFeatureStateType Swing;
+      MideaACOpFeatureStateType Vent;
+      uint32_t TimerStart;
+      uint32_t TimerStop;
+    }MideaACCommandType;
+
 class ESP8266_Midea_RS485Class {
   public:
     ESP8266_Midea_RS485Class(uint8_t re_de_pin, uint8_t master_id, uint8_t slave_id, uint8_t command_time, uint8_t response_timeout);
@@ -13,19 +74,33 @@ class ESP8266_Midea_RS485Class {
     virtual void begin(uint8_t re_de_pin, uint8_t master_id, uint8_t slave_id, uint8_t command_time, uint8_t response_timeout);
     void EnableTX();
     void EnableRX();
+    uint8_t SetMode(MideaACOpModeType mode);
+    uint8_t SetFanMode(MideaACFanModeType fan_mode);
+    uint8_t SetTemp(uint8_t temp);
+    uint8_t SetAuxHeat_Turbo(MideaACOpFeatureStateType value);
+    uint8_t SetEcho_Sleep(MideaACOpFeatureStateType value);
+    uint8_t SetSwing(MideaACOpFeatureStateType value);
+    uint8_t SetVent(MideaACOpFeatureStateType value);
     void Update();
+    void Lock();
+    void Unlock();
     MideaACPropertiesType State;
     uint8_t SentData[16];
-    uint8_t ReceivedData[32];
+    uint8_t ReceivedData[40];
   private:
     uint8_t ComControlPin;
     uint8_t SlaveId;
     uint8_t MasterId;
     uint8_t Master_Send_Time;
     uint8_t Slave_Resp_Time;
+    MideaACCommandType DesiredState;
+    uint8_t UpdateNextCycle;
   protected:
-    uint8_t CalculateCRC();
+    uint8_t CalculateCRC(uint8_t len);
     void ClearResponseBuffer();
+    uint8_t ParseResponse();
+    uint8_t CalculateSetTime(uint32_t time);
+    uint32_t CalculateGetTime(uint8_t time);
 }; 
 
 extern ESP8266_Midea_RS485Class ESP8266_Midea_RS485;  
